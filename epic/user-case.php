cd60540e9cc7d61859e009a460a28e299bd74886
Erik Young <youngblkraven@gmail.com>
2019-04-10 10:44:53 -0600

    added line 4 and 5 to user case

--- epic/user-case.php
+++ epic/user-case.php
@@ -4,8 +4,8 @@
 <p>2.Click on quantity, place number of how many you want to purchase</p>
 <p>
 	<p>3.Click update</p>
-<p>4. page updates with new quantity</p>
-<p>5.user clicks checkout</p>
+<p>4.Page updates with new quantity</p>
+<p>5.User clicks checkout</p>
 <p><a href="./index.php">Home</a><p>
 <p><a href="./persona.php">persona</a><p>
 <p><a href="./user-story.php">user-story</a><p>
