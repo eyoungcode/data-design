4e39894f5899ec4dbe9aa97f1380d3bfc3ca37ea
Erik Young <youngblkraven@gmail.com>
2019-04-10 11:05:32 -0600

    added likes, dislikes and attributes to persona

--- epic/persona.php
+++ epic/persona.php
@@ -24,6 +24,28 @@
 		<strong>Occupation</strong>
 		Construction Foreman
 	</li>
+	<li>
+	<strong>Likes:</strong>
+		<p>Playing online with friends</p>
+		<p>Riding his bike</p>
+		<p>Mountain climbing</p>
+		<p>Fast shipping</p>
+</li>
+	<li>
+	<strong>Dislikes:</strong>
+		<p>When it takes to long to get his products he purchases online</p>
+		<p>Cats *no Mewo mix for me*</p>
+		<p>People who can't take direction</p>
+		<p>Websites that take to long to load</p>
+	</li>
+	<li>
+		<strong>Attributes</strong>
+		<p>Does like to wait</p>
+		<p>Like it when he can order multiple items at once</p>
+		<p>Has $$$</p>
+		<p>Likes websites he can use across all of his devices</p>
+	</li>
+
 </ul>
 <p><a href="./index.php">Home</a><p>
 <p><a href="./user-story.php">User-story</a><p>
