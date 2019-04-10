161d8c957df8340d2804858fe2c9895e36c60522
Erik Young <youngblkraven@gmail.com>
2019-04-10 07:54:21 -0600

    change index to php

--- epic/index.php
+++ epic/index.php
@@ -7,8 +7,6 @@
 	<body>
 		<h2>User Persona</h2>
 		<img src="razer-thresher-ultimate-ps4-7.jpg" alt="gamer" style=":width: 400px; height:400px"/>
-
-
 		<h3>User Story</h3>
 		<div>
 			<h4>User Case</h4>
