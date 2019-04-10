06d7c2d181f46509471089415f0a215a14fed361
Erik Young <youngblkraven@gmail.com>
2019-04-10 10:44:06 -0600

    added line 4 and 5 to user case

--- epic/user-case.php
+++ epic/user-case.php
@@ -1,12 +1,11 @@
 <p>
 	1.Review the number of items in shopping cart
 </p>
+<p>2.Click on quantity, place number of how many you want to purchase</p>
 <p>
-	2.If you want to modify the quantity
-</p>
-<p>3.Click on quantity, place number of how many you want to purchase</p>
-<p>
-	<p>4.Click update</p>
+	<p>3.Click update</p>
+<p>4. page updates with new quantity</p>
+<p>5.user clicks checkout</p>
 <p><a href="./index.php">Home</a><p>
 <p><a href="./persona.php">persona</a><p>
 <p><a href="./user-story.php">user-story</a><p>
