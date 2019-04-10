8536b47bf7b096e34e56aaacb7b9459081c4fa9b
Erik Young <youngblkraven@gmail.com>
2019-04-10 10:10:36 -0600

    add lines items to user-case

--- epic/user-case.php
+++ epic/user-case.php
@@ -1,13 +1,12 @@
 <p>
-	Likes to buy games from Game Stop
+	1.Likes to buy games from Game Stop
 </p>
 <p>
-	Does not like to play online games alone, so he sometimes buys multiple copies to give to his gaming friends
+	2.Does not like to play online games alone, so he sometimes buys multiple copies to give to his gaming friends
 </p>
+<p>3.Goes to quality and modifies the number of games he is purchasing</p>
 <p>
-	He must fill out shipping information either as a guest or a Game Stop user
-
+	4.He must fill out shipping information either as a guest or a Game Stop user</p>
 <p><a href="./index.php">Home</a><p>
 <p><a href="./persona.php">persona</a><p>
 <p><a href="./user-story.php">user-story</a><p>
-</p>
