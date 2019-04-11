5771d066b8c7c387b035cdf73c75e3271d84262a
Erik Young <youngblkraven@gmail.com>
2019-04-11 14:47:16 -0600

    added EDR and picture to conceptual-model

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -19,7 +19,12 @@
 		<p>One shopping cart for 1 user (1 to 1)</p>
 		<p>1 user can only have 1 shopping cart (1 to 1)</p>
 	</li>
-</ul>
+	</ul>
+<p>
+
+</p>
+<li><strong>EDR Example</strong></li>
+<img src="shopping-cart.jpg" alt="shopping cart" style=":width: 400px; height:400px"/>
 
 <p><a href="./index.php">Home</a><p>
 <p><a href="./user-story.php">User-story</a><p>
