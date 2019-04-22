9929966308d33c904df585a93690406c293abe7f
Erik Young <youngblkraven@gmail.com>
2019-04-22 14:12:08 -0600

    updated inner join

--- epic/oml.sql
+++ epic/oml.sql
@@ -1,26 +1,12 @@
-create table user (
-	userId binary(16) not null,
-	userEmailAddress varchar(128),
-	userPassword varchar(128),
-	primary key(userId)
-);
 insert into user
 (userId, userEmailAddress, userPassword)
-	VALUE (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7'), 'bobjorge@gmail.com', 'bob');
-
+	VALUES (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7'), 'bobjorge@gmail.com', 'bob');
 
 update user set userPassword = 'erik' where userPassword ='bob';
 
 delete from user where userPassword = 'ted';
 
-create table cart (
-	cartId     BINARY(16) not null,
-	cartAmount BINARY(16) not null,
-	cartUserId BINARY(16) not null,
-	primary key (cartId),
-	foreign key (cartUserId) references user(userId)
-);
-		INSERT Into cart (CartId, cartAmount)VALUES (UNHEX('aeb0ecbfac0642c8a9626feb861bbd5f'), '500');
+		INSERT Into cart (CartId, cartAmount, cartUserId)VALUES (UNHEX('aeb0ecbfac0642c8a9626feb861bbd5f'), '500', (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7')));
 select cartAmount from cart where cartAmount = 5001;
 select cartId from cart inner join user on userId = (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7'));
 select cartId from cart where cartAmount = 5850;
