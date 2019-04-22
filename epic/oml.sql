420662a84ef268ae49141ccdb2cbf8aa343e9971
Erik Young <youngblkraven@gmail.com>
2019-04-22 15:53:29 -0600

    made inserts for cart and item

--- epic/oml.sql
+++ epic/oml.sql
@@ -1,14 +1,21 @@
 insert into user
 (userId, userEmailAddress, userPassword)
-	VALUES (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7'), 'bobjorge@gmail.com', 'bob');
+	values (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7'), 'bobjorge@gmail.com', 'bob');
+
+insert into cart
+(cartId, cartAmount, cartUserId)
+values (UNHEX ('e94879a28dfd47fc93b444972dff4bce'), 45854, (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7')));
+
+insert into item
+(itemId, itemAmount, itemCost)
+values (UNHEX ('57dbf733ba9645f78c926dde9f29a394'), '54', '252');
 
 update user set userPassword = 'erik' where userPassword ='bob';
 
 delete from user where userPassword = 'ted';
 
-		INSERT Into cart (CartId, cartAmount, cartUserId)VALUES (UNHEX('aeb0ecbfac0642c8a9626feb861bbd5f'), '500', (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7')));
+		insert into cart (CartId, cartAmount, cartUserId)values (UNHEX('aeb0ecbfac0642c8a9626feb861bbd5f'), '500', (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7')));
 select cartAmount from cart where cartAmount = 5001;
-select cartId from cart inner join user on userId = (UNHEX ('db58fdd75a2743e39a0d45397cd0dbc7'));
 select cartId from cart where cartAmount = 5850;
 
 
